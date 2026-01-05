2d0869bef38167c3104c52f7fd61593a75980721
Chethan Reddy Jammula <cj8719@srmist.edu.in>
2026-01-05 13:42:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.1 MB (43.93%) - LeetSync

---
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,44 @@
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> result;
+
+        // If the root is null, return an empty list
+        if (root == nullptr) {
+            return result;
+        }
+
+        // To keep track of the previously processed node
+        TreeNode* previousNode = nullptr;
+        // Stack to manage the traversal
+        stack<TreeNode*> traversalStack;
+
+        // Process nodes until both the root is null and the stack is empty
+        while (root != nullptr || !traversalStack.empty()) {
+            // Traverse to the leftmost node
+            if (root != nullptr) {
+                traversalStack.push(root);
+                root = root->left;
+            } else {
+                // Peek at the top node of the stack
+                root = traversalStack.top();
+
+                // If there is no right child or the right child was already
+                // processed
+                if (root->right == nullptr || root->right == previousNode) {
+                    result.push_back(root->val);
+                    traversalStack.pop();
+                    previousNode = root;
+                    root = nullptr;  // Ensure we don’t traverse again from this
+                                     // node
+                } else {
+                    // Move to the right child
+                    root = root->right;
+                }
+            }
+        }
+
+        return result;
+    }
+};
